eb8b795d245e5659b5a91432ebffbcd5941744cf
Karel Ha <mathemage@gmail.com>
2019-04-08 16:16:24 +0200

    Train in Keras
    - titanic/logistic_regression
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- titanic/logistic_regression.py
+++ titanic/logistic_regression.py
@@ -2,6 +2,8 @@ import keras as K
 
 # hyperparameters
 input_dim = None  # TODO
+epochs = 5
+batch_size = 32
 
 # initialize
 model = K.Sequential()
@@ -11,3 +13,6 @@ model.add(K.layers.Dense(units=1, activation='sigmoid', input_dim=input_dim))
 
 # loss
 model.compile(loss=K.losses.categorical_crossentropy, optimizer=K.optimizers.Adam)
+
+# train: x_train and y_train are Numpy arrays
+model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)  # TODO
